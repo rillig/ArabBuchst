22cbf63fc62df02942dd0127bd32f807f38313c9
Roland Illig <roland.illig@gmx.de>
2015-10-18 13:58:35 +0200

    Strenger Unittest für eingeschlichene Unicodezeichen

--- app/src/test/java/de/roland_illig/arabbuchst/TransliteratorTest.java
+++ app/src/test/java/de/roland_illig/arabbuchst/TransliteratorTest.java
@@ -2,62 +2,75 @@ package de.roland_illig.arabbuchst;
 
 import org.testng.annotations.Test;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.nio.charset.StandardCharsets;
 import java.util.Iterator;
-import java.util.List;
 
 import static org.testng.Assert.assertEquals;
 
 public class TransliteratorTest {
 
-    @Test
-    public void transliterateEverything() throws IOException {
-        List<TrainingData.WordPair> wordPairs = new TrainingData().loadWordPairs();
-        Iterator<TrainingData.WordPair> it = wordPairs.iterator();
-        if(false){
-        PrintWriter wr = new PrintWriter(new OutputStreamWriter(new FileOutputStream("translit.txt"), StandardCharsets.UTF_8));
-        while (it.hasNext()) {
-            String ar = it.next().ar;
-            wr.println(Transliterator.arabicToLatin(ar));
+    private Iterator<TrainingData.WordPair> it;
+
+    // because of http://stackoverflow.com/questions/33184779/wrong-encoding-in-unit-test-output
+    private static String showUnicode(String s) {
+        String uni = "";
+        for (char c : s.toCharArray()) {
+            if (0x0020 <= c && c <= 0x007E) {
+                uni += c;
+            } else {
+                uni += String.format("\\u%04X", (int) c);
+            }
         }
-        wr.close();}
-        assertEquals(Transliterator.arabicToLatin(it.next().ar), "qāmūs");
-        assertEquals(Transliterator.arabicToLatin(it.next().ar), "ʾālū");
-        assertEquals(Transliterator.arabicToLatin(it.next().ar), "masāʾ");
-        assertEquals(Transliterator.arabicToLatin(it.next().ar), "ʾālmānyā");
-        assertEquals(Transliterator.arabicToLatin(it.next().ar), "miṣr");
-        assertEquals(Transliterator.arabicToLatin(it.next().ar), "bayt");
-        assertEquals(Transliterator.arabicToLatin(it.next().ar), "maktab");
-        assertEquals(Transliterator.arabicToLatin(it.next().ar), "ʾārnab");
-        assertEquals(Transliterator.arabicToLatin(it.next().ar), "ʾāsad");
-        assertEquals(Transliterator.arabicToLatin(it.next().ar), "tyn");
-        assertEquals(Transliterator.arabicToLatin(it.next().ar), "jamal");
-        assertEquals(Transliterator.arabicToLatin(it.next().ar), "ḥalyb");
-        assertEquals(Transliterator.arabicToLatin(it.next().ar), "khubz");
-        assertEquals(Transliterator.arabicToLatin(it.next().ar), "dibk");
-        assertEquals(Transliterator.arabicToLatin(it.next().ar), "dhahab");
-        assertEquals(Transliterator.arabicToLatin(it.next().ar), "rādyū");
-        assertEquals(Transliterator.arabicToLatin(it.next().ar), "zaytūn");
-        assertEquals(Transliterator.arabicToLatin(it.next().ar), "samak");
-        assertEquals(Transliterator.arabicToLatin(it.next().ar), "shams");
-        assertEquals(Transliterator.arabicToLatin(it.next().ar), "ṣābūn");
-        assertEquals(Transliterator.arabicToLatin(it.next().ar), "ḍabāb");
-        assertEquals(Transliterator.arabicToLatin(it.next().ar), "ṭālib");
-        assertEquals(Transliterator.arabicToLatin(it.next().ar), "ẓarf");
-        assertEquals(Transliterator.arabicToLatin(it.next().ar), "ʿyd");
-        assertEquals(Transliterator.arabicToLatin(it.next().ar), "ġāly");
-        assertEquals(Transliterator.arabicToLatin(it.next().ar), "fyl");
-        assertEquals(Transliterator.arabicToLatin(it.next().ar), "qalam");
-        assertEquals(Transliterator.arabicToLatin(it.next().ar), "kitāb");
-        assertEquals(Transliterator.arabicToLatin(it.next().ar), "lybyā");
-        assertEquals(Transliterator.arabicToLatin(it.next().ar), "maktab");
-        assertEquals(Transliterator.arabicToLatin(it.next().ar), "nūr");
-        assertEquals(Transliterator.arabicToLatin(it.next().ar), "hāl");
-        assertEquals(Transliterator.arabicToLatin(it.next().ar), "yāsmyn");
+        return uni;
+    }
+
+    @Test
+    public void transliterateEverything() {
+        it = new TrainingData().loadWordPairs().iterator();
+        assertNext("qāmūs");
+        assertNext("ʾālū");
+        assertNext("masāʾ");
+        assertNext("ʾālmānyā");
+        assertNext("miṣr");
+        assertNext("maktab");
+        assertNext("ʾārnab");
+        assertNext("ʾāsad");
+        assertNext("bayt");
+        assertNext("tyn");
+        assertNext("jamal");
+        assertNext("ḥalyb");
+        assertNext("khubz");
+        assertNext("dibk");
+        assertNext("dhahab");
+        assertNext("rādyū");
+        assertNext("zaytūn");
+        assertNext("samak");
+        assertNext("shams");
+        assertNext("ṣābūn");
+        assertNext("ḍabāb");
+        assertNext("ṭālib");
+        assertNext("ẓarf");
+        assertNext("ʿyd");
+        assertNext("ġāly");
+        assertNext("fyl");
+        assertNext("qalam");
+        assertNext("kitāb");
+        assertNext("lybyā");
+        assertNext("maktab");
+        assertNext("nūr");
+        assertNext("hāl");
+        assertNext("yāsmyn");
+        assertNext("bayrūt");
+        assertNext("āithnān");
+        assertNext("ʿishrūna");
+        assertNext("ḍayif");
+        assertNext("baġdād");
+        assertNext("milḥ");
         assertEquals(it.hasNext(), false);
-   }
+    }
+
+    private void assertNext(String expected) {
+        String transliterated = Transliterator.arabicToLatin(it.next().ar);
+        assertEquals(showUnicode(transliterated.replaceAll("[ -~\u00C0-\u02FF\u1E00-\u1EFF]", "")), "", "Illegal chars");
+        assertEquals(showUnicode(transliterated), showUnicode(expected));
+    }
 }
